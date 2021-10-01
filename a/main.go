db030cdcbe2de621f83cea3c72e73091f903b268
anindyadas93 <37060139+anindyadas93@users.noreply.github.com>
2021-10-01 22:41:54 +0530

    Test

--- a/main.go
+++ a/main.go
@@ -1,7 +1,12 @@
 package a
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
 
 func A() {
 	fmt.Println("Package A")
+	color.Cyan("Prints text in cyan.")
 }
--- b/main.go
+++ b/main.go
@@ -1,7 +1,12 @@
 package b
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
 
 func B() {
 	fmt.Println("Package B")
+	color.Red("Prints text in red.")
 }
